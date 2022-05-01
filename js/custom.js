1fc756bcf5d011fa121cf2b31a76580a6ddc80dc
MehranBahrabadi <bahrabadimehran72@gmail.com>
2022-05-01 16:16:36 +0430

    design header

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    let menu = document.getElementById('menu');
+    if (menu.className === "header-menu") {
+        menu.className += " show";
+    } else {
+        menu.className = "header-menu";
+    }
+}
